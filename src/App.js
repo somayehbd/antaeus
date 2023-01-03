9022946230c991dcfc6e8e2bc83ee2dce63e9507
somayehbd <somayehbd@gmail.com>
2023-01-03 11:51:07 +0100

    Added SigninResult component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import './App.css';
+import SigninResult from './components/signinresult/SigninResult';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />} />
+        <Route path="/signin-result" element={<SigninResult />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/signinresult/SigninResult.jsx
@@ -0,0 +1,6 @@
+function SigninResult(){
+    return(
+        <></>
+    )
+}
+export default SigninResult;
