2f5e0d8714e3ce0d5c50620a29064fc83d825a51
somayehbd <somayehbd@gmail.com>
2023-01-04 19:37:05 +0100

    Added real time inside  MarketStatus component.

--- src/components/MarketStatus/MarketStatus.jsx
+++ src/components/MarketStatus/MarketStatus.jsx
@@ -1,14 +1,28 @@
 import { RiComputerFill } from 'react-icons/ri';
+import { useState, useEffect } from 'react';
 import style from "./MarketStatus.module.css"
 
 function MarketStatus() {
-    return (
-    <>
-        <div className={style.status}><div className={ style.icon }><RiComputerFill className={`${style.online}` }/></div> <div className={style.time}>10:20 AM</div></div>
-    </>
-    )
+
+    
+        const [date, setDate] = useState(new Date());
+
+        function refreshClock() {
+            setDate(new Date());
+        }
+        useEffect(() => {
+            const timerId = setInterval(refreshClock, 1000);
+        }, []);
+
+
+        return (
+            <>
+                <div className={style.status}><div className={style.icon}><RiComputerFill className={`${style.online}`} /></div> <div className={style.time}>{date.toLocaleTimeString()}</div></div>
+            </>
+        )
 
 
 
+   
 }
-export default MarketStatus;
+    export default MarketStatus;
