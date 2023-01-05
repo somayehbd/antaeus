e15135d700551935813e7a06bb0878cf650fe80c
somayehbd <somayehbd@gmail.com>
2023-01-05 15:05:10 +0100

    Added Profile component.

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,10 @@
+import style from "./Profile.module.css"
+
+function Profile() {
+    return (
+        <>
+            <span class={style.dot}></span>
+        </>
+    )
+}
+export default Profile;
