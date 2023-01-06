01132ca662455dc3778f3cc53f11b6ddf3924fb5
somayehbd <somayehbd@gmail.com>
2023-01-07 00:19:39 +0100

    Added profile menu.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import style from "./Profile.module.css"
 
 function Profile() {
+    const [name, setName] = useState("");
+    const [show, setShow] = useState(false);
+
+    function onclick() {
+        setShow(!show)
+    }
+
+    useEffect(() => {
+        const customerInfo = localStorage.getItem('CustomerInfo');
+        const customerInfoObj = JSON.parse(customerInfo);
+        setName(`${customerInfoObj.name} ${customerInfoObj.family}`);
+    }, []);
+
     return (
         <>
-            <span class={style.dot}></span>
+            <span class={style.dot} onClick={() => { onclick() }}></span>
+            {
+            show == true ? <div className={style.ProfileMenu}>
+                <div className={style.picture}> </div>
+                <ul className={style.Profilecontent}>
+                    <li>{name}</li>
+                    <li><a href="https://sso.eltak.ir/account/logout">Log out</a></li>
+                </ul>
+            </div> : null
+            }
         </>
     )
 }
