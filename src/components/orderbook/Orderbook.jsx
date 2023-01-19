0f7c7db7469879125b75a46eb01427447ff4b4ed
somayehbd <somayehbd@gmail.com>
2023-01-19 20:04:02 +0100

    feat: added orderbook component

--- src/components/orderbook/Orderbook.jsx
+++ src/components/orderbook/Orderbook.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import useOrderbook from "../../hooks/useOrderbook";
 import style from "./Orderbook.module.css"
 function Orderbook() {
+    const [orderbookApiResponse] = useOrderbook()
+    const [orderbook, setOrderbook] = useState([])
+
+    function CombineBidAsk(asks, bids) {
+        const result = [];
+
+        if (asks == null || asks == undefined)
+            return result;
+
+        for (let i = 0; i < asks.length; i++) {
+            const item = { ask: asks[i], bid: bids[i] }
+            result.push(item)
+        }
+
+        return result;
+
+    }
+
+    useEffect(() => {
+        const newOrderbook = CombineBidAsk(orderbookApiResponse.asks, orderbookApiResponse.bids);
+        setOrderbook(newOrderbook)
+    }, [orderbookApiResponse]);
+
     return (
         <>
             <table class={`table ${style.orderbooktable}`} >
@@ -10,30 +35,22 @@ function Orderbook() {
                         <th>Price</th>
                         <th>Price</th>
                         <th>Quantity</th>
+                        <th >Total Quantity</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>355.111</td>
-                        <td>355.111</td>
-                        <td>355.111</td>
-                        <td>355.111</td>
-                        <td>355.111</td>
-                    </tr>
-                    <tr>
-                        <td>355.111</td>
-                        <td>355.111</td>
-                        <td>355.111</td>
-                        <td>355.111</td>
-                        <td>355.111</td>
-                    </tr>
-                    <tr>
-                        <td>355.111</td>
-                        <td>355.111</td>
-                        <td>355.111</td>
-                        <td>355.111</td>
-                        <td>355.111</td>
-                    </tr>
+                    {
+                        orderbook.map(item => {
+                            return (<tr>
+                                <td>{item.ask.totalQuantity}</td>
+                                <td>{item.ask.quantity}</td>
+                                <td>{item.ask.price}</td>
+                                <td>{item.bid.price}</td>
+                                <td>{item.bid.quantity}</td>
+                                <td>{item.bid.totalQuantity}</td>
+                            </tr>)
+                        })
+                    }
                 </tbody>
             </table>
         </>
