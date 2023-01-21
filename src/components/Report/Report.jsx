b92fbda5014936c757c3bc679a68fea2190d774a
somayehbd <somayehbd@gmail.com>
2023-01-21 23:05:58 +0100

    fix: change class to className in report component

--- src/components/Report/Report.jsx
+++ src/components/Report/Report.jsx
@@ -8,17 +8,17 @@ function Report() {
     return (
         <>
             <ul className={`nav nav-pills ${style.navpill}`} id="pills-tab" role="tablist">
-                <li class="nav-item" role="presentation">
+                <li className="nav-item" role="presentation">
                     <button className="nav-link active" id="pills-Orders-tab" data-toggle="pill" data-target="#pills-Orders" type="button" role="tab" aria-controls="pills-Orders" aria-selected="true">Orders</button>
                 </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="pills-transactions-tab" data-toggle="pill" data-target="#pills-transactions" type="button" role="tab" aria-controls="pills-transactions" aria-selected="false">transactions</button>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link" id="pills-transactions-tab" data-toggle="pill" data-target="#pills-transactions" type="button" role="tab" aria-controls="pills-transactions" aria-selected="false">transactions</button>
                 </li>
-                <li class="nav-item" role="presentation">
+                <li className="nav-item" role="presentation">
                 </li>
             </ul>
-            <div class={`tab-content ${style.tabContentReport}`} id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-Orders" role="tabpanel" aria-labelledby="pills-Orders-tab">
+            <div className={`tab-content ${style.tabContentReport}`} id="pills-tabContent">
+                <div className="tab-pane fade show active" id="pills-Orders" role="tabpanel" aria-labelledby="pills-Orders-tab">
                     <table className={`table ${style.table}`}>
                         <thead>
                             <tr>
@@ -50,9 +50,8 @@ function Report() {
 
                         </tbody>
                     </table></div>
-                <div class="tab-pane fade" id="pills-transactions" role="tabpanel" aria-labelledby="pills-transactions-tab">...</div>
+                <div className="tab-pane fade" id="pills-transactions" role="tabpanel" aria-labelledby="pills-transactions-tab">...</div>
             </div>
-
         </>
     )
 }
