878fbe0780b7f72054dfd0b1c1daf3a26e79fa19
somayehbd <somayehbd@gmail.com>
2023-01-22 20:20:48 +0100

    feat: use useOverview hook to fill overview component

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -1,13 +1,14 @@
 import PieChartComponent from 'antaeus.components.piechart';
 import style from './Overview.module.css'
+import useOverview from '../../hooks/useOverview';
 
 function Overview() {
+    const [overview] = useOverview();
+    console.log(overview)
     return (
 
         <div className={style.flexContainer}>
-
-            <div className={style.item1}><PieChartComponent label1="Blocked: " value1="2517.5" label2="Withdrawable: " value2="18714.37" postfix="€" color="#fff" /> </div>
-
+            <div className={style.item1}><PieChartComponent label1="Blocked: " value1={overview.blocked} label2="Withdrawable: " value2={overview.withdrawable} postfix="€" color="#fff" /> </div>
         </div>
 
     )
