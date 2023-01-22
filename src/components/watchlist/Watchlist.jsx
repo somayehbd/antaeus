3929ca0d3e866c0876c15b3d8dff884bf994a341
somayehbd <somayehbd@gmail.com>
2023-01-22 20:20:04 +0100

    feat: get selected watchlist items

--- src/components/watchlist/Watchlist.jsx
+++ src/components/watchlist/Watchlist.jsx
@@ -6,11 +6,33 @@ import { GrFormSubtract } from "react-icons/gr";
 import { MdEdit } from "react-icons/md";
 import { useEffect } from "react";
 import { useState } from "react";
+import axios from "axios";
 
 function Watchlist() {
     const [watchlistLightweight] = useWatchListLightweight();
-    const [watchList] = useWatchListById();
+    let [watchList] = useWatchListById(1);
     const [watchListItems, setWatchListItems] = useState([])
+
+    const onWatchlistChange = (event) => { 
+        const token = localStorage.getItem("AccessToken");
+        axios.get(`https://ot-api.eltak.ir/WatchList/WatchList/${event.target.value}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(
+            response => {
+            console.log(response)
+                if (response.data.status == false) {
+                    alert(response.data.errorMessage);
+                } else {
+                    setWatchListItems(response.data.data.watchListItems)
+                }
+            },
+            reason => {
+                if (reason.response.status == 401)
+                    window.location.href = "https://ot-api.eltak.ir/bff/login";
+            });
+    }
 
     useEffect(() => {
         if (watchList.watchListItems == null || watchList.watchListItems == undefined)
@@ -31,7 +53,7 @@ function Watchlist() {
                 <div className="col-12">
                     <div className={style.flexcontainer}>
                         <div className={style.item1}>
-                            <select className={`form-control form-control-sm ${style.selectbuton}`}>
+                            <select onChange={onWatchlistChange} className={`form-control form-control-sm ${style.selectbuton}`}>
                                 {watchlistLightweight.map(item => {
                                     return (<option key={item.id} value={item.id}>{item.name}</option>)
                                 })}
