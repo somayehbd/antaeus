37ee4c03ca26e3eedc9e808d4b0c149e1bd4c445
somayehbd <somayehbd@gmail.com>
2023-01-22 20:19:49 +0100

    feat: add id to useWatchlistBtId hook

--- src/hooks/useWatchListById.jsx
+++ src/hooks/useWatchListById.jsx
@@ -1,7 +1,7 @@
 import useGetAPI from "./useGetAPI";
 
-function useWatchListById() {
-    const [watchList] = useGetAPI('https://ot-api.eltak.ir/WatchList/WatchList/1', {})
+function useWatchListById(id) {
+    const [watchList] = useGetAPI(`https://ot-api.eltak.ir/WatchList/WatchList/${id}`, {})
     return ([watchList])
 }
 export default useWatchListById;
