4d5d08f34cffeff35b0b2bd92067dfb65dd72564
somayehbd <somayehbd@gmail.com>
2023-07-28 13:42:10 +0200

    feat: Added Data Fetching in Overview Component

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -1,13 +1,38 @@
 import PieChartComponent from 'antaeus.components.piechart';
+import CONFIG from '../../config';
 import style from './Overview.module.css'
+import { useEffect, useState } from 'react';
 
 function Overview() {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        
+        const accessToken = localStorage.getItem('Token')
+        fetch(`${CONFIG.BASE_ADDRESS}/WalletManagement/Wallet`, {
+            headers: {
+                'Authorization': ` Bearer ${accessToken}`
+            }
+        })
+
+            .then(response => {
+                return response.json()
+            })
+
+            .then(data => {
+                setData(data.data)
+            })
+
+    }, [])
+
+
     return (
-
-        <div className={style.flexContainer}>
-            <div className={style.item1}><PieChartComponent label1="Blocked: " value1={100} label2="Withdrawable: " value2={200} postfix="€" color="#fff" /> </div>
-        </div>
-
+        <>
+            {console.log(data)}
+            <div className={style.flexContainer}>
+                <div className={style.item1}><PieChartComponent label1="Blocked:" value1={data.blocked} label2="Withdrawable: " value2={data.withdrawable} postfix="€" color="#fff" /> </div>
+            </div>
+        </>
     )
 }
 export default Overview;
