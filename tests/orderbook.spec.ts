8e5545f7167b8d2256ceda7f0b7b9d640219e2d3
somayehbd <somayehbd@gmail.com>
2023-08-18 22:36:27 +0200

    test(orderbook): Updated End-to-End test for Orderbook component

--- tests/orderbook.spec.ts
+++ tests/orderbook.spec.ts
@@ -1,37 +1,24 @@
 import { test, expect } from '@playwright/test';
 
 test('it should contain 5 table rows', async ({ page }) => {
-
   await page.goto('http://localhost:3000/', { timeout: 60000 });
-  await page.waitForURL('https://ids.kub.aghdam.nl/Account/Login**', { timeout: 120000 });
-
-  await page.getByPlaceholder('Username').click();
-  await page.getByPlaceholder('Username').type('logon');
-
-  await page.getByPlaceholder('Password').click();
-  await page.getByPlaceholder('Password').type('Pass123$');
-
-  await page.getByRole('button', { name: 'Login' }).click();
-  await page.waitForURL('http://localhost:3000/');
   await page.waitForLoadState('networkidle');
 
-  const rowsLocator = page.locator("[class*='Orderbook_orderbooktable'] tbody tr ");
-  const rowsCount = await rowsLocator.count();
-
+  const rowLocators = page.getByTestId('orderbook').locator("tbody tr");
+  const rowsCount = await rowLocators.count();
   for (let i = 0; i < rowsCount; i++) {
-    console.log(`Row number: ${i + 1}`);
-    const rowLocator = rowsLocator.nth(i);
-
+    const rowLocator = rowLocators.nth(i);
     const cellsLocator = rowLocator.locator("td");
-    const sum = await cellsLocator.count()
+    const cellsCount = await cellsLocator.count()
 
     for (let j = 0; j < 6; j++) {
       const cellContent = await cellsLocator.nth(j).textContent();
       const parsedNumber = Number(cellContent);
       expect(isNaN(parsedNumber)).toBe(false);
     }
+
+    expect(rowsCount).toEqual(5);
+    expect(cellsCount).toEqual(6);
+
   }
-
-  expect(rowsCount).toEqual(5);
-
 });
