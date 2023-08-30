0b0763b344750461eb2fc5112fbc5ed4d6ca8bde
somayehbd <somayehbd@gmail.com>
2023-08-30 22:22:19 +0200

    feat: Added symbol ID fetching and display in Search component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,17 +1,41 @@
 import { Typeahead } from 'react-bootstrap-typeahead';
+import CONFIG from '../../config';
+import { useState, useEffect } from 'react';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import style from "./Search.module.css"
 
 function Search() {
+
+    const [symbolIds, setsymbolIds] = useState([]);
+
+    useEffect(() => {
+        fetch(`${CONFIG.BASE_ADDRESS}/MarketData/Symbol/Lightweight`)
+            .then(response => {
+                return response.json();
+            })
+
+            .then(data => {
+
+                setsymbolIds(data.data)
+
+            })
+
+            .catch(error => {
+                console.log('eroooor')
+
+            })
+    }, []);
+
     return (
         <>
             <div className={style.search}>
                 <Typeahead
-                    id="symbolId"
-                    labelKey='symbolId'
+                    id=""
+                    labelKey={option => `${option.symbolId}`}
+                    options={symbolIds}
                     placeholder="Choose a symbol ..."
                     size='sm'
-                />
+                />  
             </div>
         </>
     )
